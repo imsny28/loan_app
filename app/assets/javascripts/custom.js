153a66d1578fa73cef8d9eb199e5c1a083dc5622
Fateh Singh <fatehsingh.sunny5@gmail.com>
2019-09-29 16:42:05 +0530

    changes for alert message

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
    $('.ui.dropdown') .dropdown();
+
+   $('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
 });
